e14e71cc42a9ddebf15cf08179b841a8222fe9a5
Lucas Kenzo Kuroki <lcskenzoo@gmail.com>
2024-08-07 14:23:11 -0300

    update object tracking
    .

--- Teste com detector de cores/ColorObjectTracking.py
+++ Teste com detector de cores/ColorObjectTracking.py
@@ -1,23 +1,34 @@
+from djitellopy import Tello
 import cv2
 import numpy as np
 
-frameWidth = 640
-frameHeight = 480
-cap = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
+width = 1280
+height = 720
+deadZone =50
+startCounter =0
 
-deadZone=100
+me = Tello()
+me.connect()
+me.for_back_velocity = 0
+me.left_right_velocity = 0
+me.up_down_velocity = 0
+me.yaw_velocity = 0
+me.speed = 0
+
+print(me.get_battery())
+me.streamoff()
+me.streamon()
+frameWidth = width
+frameHeight = height
+
 global imgContour
 
-def empty(a):
-    pass
-
+#ajustar cor
 cv2.namedWindow("HSV")
 cv2.resizeWindow("HSV",640,240)
-cv2.createTrackbar("HUE Min","HSV",19,179,empty)
-cv2.createTrackbar("HUE Max","HSV",35,179,empty)
-cv2.createTrackbar("SAT Min","HSV",107,255,empty)
+cv2.createTrackbar("HUE Min","HSV",20,179,empty)
+cv2.createTrackbar("HUE Max","HSV",40,179,empty)
+cv2.createTrackbar("SAT Min","HSV",148,255,empty)
 cv2.createTrackbar("SAT Max","HSV",255,255,empty)
 cv2.createTrackbar("VALUE Min","HSV",89,255,empty)
 cv2.createTrackbar("VALUE Max","HSV",255,255,empty)
@@ -26,9 +37,9 @@ cv2.namedWindow("Parameters")
 cv2.resizeWindow("Parameters",640,240)
 cv2.createTrackbar("Threshold1","Parameters",166,255,empty)
 cv2.createTrackbar("Threshold2","Parameters",171,255,empty)
-cv2.createTrackbar("Area","Parameters",3750,30000,empty)
+cv2.createTrackbar("Area","Parameters",1750,30000,empty)
 
-
+#função de exibição dos videos
 def stackImages(scale,imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
@@ -60,60 +71,61 @@ def stackImages(scale,imgArray):
         ver = hor
     return ver
 
+#funcao pega contorno e atualiza a variavel de voo
 def getContours(img,imgContour):
-
+    global dir
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        areaMin = cv2.getTrackbarPos("Area", "Parameters")
+        areaMin = cv2.getTrackbarPos("Area", "Parameters")/5
         if area > areaMin:
             cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
-            print(len(approx))
+            #print(len(approx))
             x , y , w, h = cv2.boundingRect(approx)
-            cv2.rectangle(imgContour, (x , y ), (x + w , y + h ), (0, 255, 0), 5)
-
-            cv2.putText(imgContour, "Points: " + str(len(approx)), (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, .7,
-                        (0, 255, 0), 2)
-            cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX, 0.7,
-                        (0, 255, 0), 2)
-            cv2.putText(imgContour, " " + str(int(x))+ " "+str(int(y)), (x - 20, y- 45), cv2.FONT_HERSHEY_COMPLEX, 0.7,
-                        (0, 255, 0), 2)
-
-            cx = int(x + (w / 2))
-            cy = int(y + (h / 2))
+            cx = int(x + (w / 2))  # CENTER X OF THE OBJECT
+            cy = int(y + (h / 2))  # CENTER X OF THE OBJECT
 
             if (cx <int(frameWidth/2)-deadZone):
                 cv2.putText(imgContour, " GO LEFT " , (20, 50), cv2.FONT_HERSHEY_COMPLEX,1,(0, 0, 255), 3)
                 cv2.rectangle(imgContour,(0,int(frameHeight/2-deadZone)),(int(frameWidth/2)-deadZone,int(frameHeight/2)+deadZone),(0,0,255),cv2.FILLED)
+                dir = 1
             elif (cx > int(frameWidth / 2) + deadZone):
                 cv2.putText(imgContour, " GO RIGHT ", (20, 50), cv2.FONT_HERSHEY_COMPLEX,1,(0, 0, 255), 3)
                 cv2.rectangle(imgContour,(int(frameWidth/2+deadZone),int(frameHeight/2-deadZone)),(frameWidth,int(frameHeight/2)+deadZone),(0,0,255),cv2.FILLED)
+                dir = 2
             elif (cy < int(frameHeight / 2) - deadZone):
                 cv2.putText(imgContour, " GO UP ", (20, 50), cv2.FONT_HERSHEY_COMPLEX,1,(0, 0, 255), 3)
                 cv2.rectangle(imgContour,(int(frameWidth/2-deadZone),0),(int(frameWidth/2+deadZone),int(frameHeight/2)-deadZone),(0,0,255),cv2.FILLED)
+                dir = 3
             elif (cy > int(frameHeight / 2) + deadZone):
                 cv2.putText(imgContour, " GO DOWN ", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1,(0, 0, 255), 3)
                 cv2.rectangle(imgContour,(int(frameWidth/2-deadZone),int(frameHeight/2)+deadZone),(int(frameWidth/2+deadZone),frameHeight),(0,0,255),cv2.FILLED)
+                dir = 4
+            else: dir=0
 
-
-            cv2.line(imgContour, (int(frameWidth/2),int(frameHeight/2)), (cx,cy),
-                     (0, 0, 255), 3)
+            cv2.line(imgContour, (int(frameWidth/2),int(frameHeight/2)), (cx,cy),(0, 0, 255), 3)
+            cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 5)
+            cv2.putText(imgContour, "Points: " + str(len(approx)), (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, .7,(0, 255, 0), 2)
+            cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX, 0.7,(0, 255, 0), 2)
+            cv2.putText(imgContour, " " + str(int(x)) + " " + str(int(y)), (x - 20, y - 45), cv2.FONT_HERSHEY_COMPLEX,0.7,(0, 255, 0), 2)
+        else: dir=0
 
 def display(img):
     cv2.line(img,(int(frameWidth/2)-deadZone,0),(int(frameWidth/2)-deadZone,frameHeight),(255,255,0),3)
     cv2.line(img,(int(frameWidth/2)+deadZone,0),(int(frameWidth/2)+deadZone,frameHeight),(255,255,0),3)
-
     cv2.circle(img,(int(frameWidth/2),int(frameHeight/2)),5,(0,0,255),5)
     cv2.line(img, (0,int(frameHeight / 2) - deadZone), (frameWidth,int(frameHeight / 2) - deadZone), (255, 255, 0), 3)
     cv2.line(img, (0, int(frameHeight / 2) + deadZone), (frameWidth, int(frameHeight / 2) + deadZone), (255, 255, 0), 3)
 
 while True:
-
-    _, img = cap.read()
+    frame_read = me.get_frame_read()
+    myFrame = frame_read.frame
+    framee = cv2.cvtColor(myFrame, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(framee, (width, height))
     imgContour = img.copy()
-    imgHsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     h_min = cv2.getTrackbarPos("HUE Min","HSV")
     h_max = cv2.getTrackbarPos("HUE Max", "HSV")
@@ -121,7 +133,6 @@ while True:
     s_max = cv2.getTrackbarPos("SAT Max", "HSV")
     v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
     v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
-    print(h_min)
 
     lower = np.array([h_min,s_min,v_min])
     upper = np.array([h_max,s_max,v_max])
@@ -139,11 +150,34 @@ while True:
     getContours(imgDil, imgContour)
     display(imgContour)
 
-    stack = stackImages(0.7,([img,result],[imgDil,imgContour]))
+    #inicia voo
+    if startCounter == 0:
+       me.takeoff()
+       startCounter = 1
 
+
+    if dir == 1:
+       me.yaw_velocity = -30
+    elif dir == 2:
+       me.yaw_velocity = 30
+    elif dir == 3:
+       me.up_down_velocity= 0
+    elif dir == 4:
+       me.up_down_velocity= 0
+    else:
+       me.left_right_velocity = 0; me.for_back_velocity = 0;me.up_down_velocity = 0; me.yaw_velocity = 0
+
+   #Envia parametros de voo
+    if me.send_rc_control:
+       me.send_rc_control(me.left_right_velocity, me.for_back_velocity, me.up_down_velocity, me.yaw_velocity)
+    #print(dir)
+
+    #hud de imagens e deteccao
+    stack = stackImages(0.9, ([img, result], [imgDil, imgContour]))
     cv2.imshow('Horizontal Stacking', stack)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        me.land()
         break
 
-cap.release()
 cv2.destroyAllWindows()
