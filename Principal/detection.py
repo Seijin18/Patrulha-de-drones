23975f021f86419d1c2d156d462a79ee0c2ad3f4
Lucas Moura da Silva <lucas.silva16@unifesp.br>
2024-08-14 14:55:43 -0300

    feat: detecção dos objetos e controle do drone

---
+++ Principal/detection.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+# variaveis =
+# Define range for the color red
+#lower_red = np.array([160, 60, 60])
+#upper_red = np.array([179, 255, 255])
+
+# Define range for the color red
+lower_red = np.array([0, 160, 100])  # Lower bound for the color
+upper_red = np.array([50, 255, 255])  # Upper bound for the color
+
+min_contour_area = 1000  # Minimum contour area to consider
+
+# Função de detecção de objetos
+def detect_objects(frame):
+    # Correct saturation
+    frame = cv2.convertScaleAbs(frame, alpha=1.5, beta=0)
+
+    # Apply Gaussian blur to reduce noise
+    frame = cv2.GaussianBlur(frame, (5, 5), 0)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Create a filter for the color red
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    # Apply the filter to the image
+    filteredFrame = cv2.bitwise_and(frame, frame, mask=mask)
+
+    # Find contours of red objects
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Filter contours based on area
+    #min_contour_area = 1000  # Minimum contour area to consider
+    large_contours = [contour for contour in contours if cv2.contourArea(contour) > min_contour_area]
+
+    area, centers = 0, (None, None)
+
+    for contour in large_contours:
+        # Draw the contour
+        epsilon = 0.022 * cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, epsilon, True)
+        cv2.drawContours(filteredFrame, [approx], -1, (0, 255, 0), 2)
+
+        # Compute the center of the contour
+        M = cv2.moments(contour)
+        if M["m00"] != 0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            cv2.circle(filteredFrame, (cX, cY), 3, (255, 255, 255), -1)
+            if area < cv2.contourArea(contour):
+                area = cv2.contourArea(contour)
+                centers = (cX, cY)
+
+    return filteredFrame, centers
--- Principal/main.py
+++ Principal/main.py
@@ -1,9 +1,39 @@
-from rotes import Rotes
+from djitellopy import Tello
+import cv2
+from controle import control
+from detection import detect_objects
+
+drone = Tello()
 
 def main():
-    rote = Rotes()
-    rote.begin_vision()
-    rote.disconnect()
-
+    drone.connect()
+    drone.streamon()
+    
+    drone.takeoff()
+    
+    battery = drone.get_battery()
+    print(f"battery is {battery}")
+    
+    while True and battery > 7:
+        frame = drone.get_frame_read().frame
+        
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # type: ignore
+        cv2.imshow('frame', frame)
+        
+        filteredFrame, centers = detect_objects(frame)
+        
+        if centers != (None, None):
+            filteredFrame = control(filteredFrame, centers, drone)
+        cv2.imshow('filteredFrame', filteredFrame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    drone.land()
+    drone.streamoff()
+    cv2.destroyAllWindows()
+    drone.end()
+    
+    
 if __name__ == "__main__":
     main()
