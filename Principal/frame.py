2c79e1cf6cf1d6a9bcdbcff9f48ff5f1257a736e
Lucas Moura da Silva <lucas.silva16@unifesp.br>
2024-08-27 20:07:46 -0300

    fix: falta do script frame

---
+++ Principal/frame.py
@@ -0,0 +1,36 @@
+from djitellopy import Tello
+import cv2
+
+def chose_frame_generation(frame_generation: int):
+    
+    if frame_generation:
+        drone = Tello()
+        drone.connect()
+        drone.streamon()
+        
+        if int(input("deseja que o drone levante vou? sim[1] não[0]: ")):
+            drone.takeoff()
+        
+        return drone
+    else:
+        webcam = cv2.VideoCapture(0)
+        return webcam
+    
+def get_frame(frame_generation: int, object_frame):
+    if frame_generation:
+        frame = object_frame.get_frame_read().frame
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    else:
+        _, frame = object_frame.read()
+        
+    return frame
+
+def close_frame_generation(frame_generation: int, object_frame):
+    if frame_generation:
+        object_frame.streamoff()
+        if object_frame.is_flying:
+            object_frame.land()
+    else:
+        object_frame.release()
+        
+    cv2.destroyAllWindows()
