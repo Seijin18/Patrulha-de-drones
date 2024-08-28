713cf15d9f80acadb4cbbcb5404ac7d4a177ed00
Marcos Seiji Shiba <marcos.shiba@unifesp.br>
2024-08-28 14:52:16 -0300

    Arrows

--- Detector de formas/arrow detection/processArrow.py
+++ Detector de formas/arrow detection/processArrow.py
@@ -3,38 +3,73 @@ import numpy as np
 import os
 
 def processArrow(image):
-    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 1)
-    img_canny = cv2.Canny(img_blur, 50, 50)
+    img_blur = cv2.GaussianBlur(image, (5, 5), 1)  # Apply Gaussian blur to the grayscale image
+    hsv = cv2.cvtColor(img_blur, cv2.COLOR_BGR2HSV)  # Convert the original image to HSV
+    cv2.imshow('hsv', img_blur)
 
+    lower_red = np.array([160, 30, 30])
+    upper_red = np.array([180, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)  # Create a mask for the red color in the HSV image
+
+    cv2.imshow('mask', mask)
+
+    img_canny = cv2.Canny(mask, 100, 200)  # Apply Canny edge detection to the mask
     return img_canny
 
-def find_tip(points, convex_hull):
-    length = len(points)
-    indices = np.setdiff1d(range(length), convex_hull)
+def skeletonize(image):
+    
+    size = np.size(image)
+    skel = np.zeros(image.shape, np.uint8)
+    ret, img = cv2.threshold(image, 127, 255, 0)
+    element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+    done = False
 
-    for i in range(2):
-        j = indices[i] + 2
-        if j > length - 1:
-            j = length - j
-        if np.all(points[j] == points[indices[i - 1] - 2]):
-            return tuple(points[j])
-        
-def getContours(image):
-    contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for contour in contours:
-        peri = cv2.arcLength(contours[0], True)
-        approx = cv2.approxPolyDP(contours[0], 0.02 * peri, True)
-        hull = cv2.convexHull(approx, returnPoints=False)
-        sides = len(hull)
+    while not done:
+        eroded = cv2.erode(img, element)
+        temp = cv2.dilate(eroded, element)
+        temp = cv2.subtract(img, temp)
+        skel = cv2.bitwise_or(skel, temp)
+        img = eroded.copy()
 
-        if sides < 3 and sides + 2 == len(approx):
-            arrow_tip = find_tip(approx[:,0,:], hull)
-            if arrow_tip:
-                cv2.circle(image, arrow_tip, 3, (255, 255, 255), -1)
-                cv2.putText(image, f"{arrow_tip}", arrow_tip, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                cv2.drawContours(image, [approx], -1, (0, 255, 0), 2)
-    return image
+        zeros = size - cv2.countNonZero(img)
+        if zeros == size:
+            done = True
+
+    return skel
+
+def get_direction(corners, contour):
+    # Get the center of the contour
+    M = cv2.moments(contour)
+    x_center = int(M["m10"] / M["m00"])
+    y_center = int(M["m01"] / M["m00"])
+
+    # Get the vector from the center to the maximum corner
+    if np.mean(corners[:, 0, 0]) < x_center:
+        direction = 'RIGHT'
+    else:
+        direction = 'LEFT'
+
+    if np.mean(corners[:, 0, 1]) < y_center:
+        direction = 'DOWN'
+    else:
+        direction = 'UP'
+
+    return direction
+    
+
+def get_vertices(contour):
+    # GoodfeaturestoTrack
+    corners = cv2.goodFeaturesToTrack(contour, 7, 0.9, 50)
+
+    for i in corners:
+        x, y = i.ravel()
+        x, y = int(x), int(y)  # Ensure x and y are integers
+        cv2.circle(image, (x, y), 3, (0, 0, 0), -1)
+        cv2.putText(image, f"{x}, {y}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
+
+    cv2.imshow('image', image)
+    return corners
 
 
 if __name__ == "__main__":
@@ -47,8 +82,12 @@ if __name__ == "__main__":
     image = cv2.imread("arrow2.jpg")
     cv2.imshow("Original Image", image)
     processedImage = processArrow(image)
-    contoursImage = getContours(processedImage)
+    get_vertices = get_vertices(processedImage)
+    direction = get_direction(get_vertices, processedImage)
+    print(f"Direction: {direction}")
+
+    skeleton = skeletonize(image)
+    cv2.imshow('skeleton', skeleton)
     cv2.imshow("Processed Image", processedImage)
-    cv2.imshow("Contours Image", contoursImage)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- Principal/tempCodeRunnerFile.py
+++ Principal/tempCodeRunnerFile.py
@@ -1,77 +1,284 @@
-from djitellopy import Tello
 import cv2
 import numpy as np
+from math import atan2, degrees, pi
 
-def detect_triangles(frame):
-    # Correct saturation
-    frame = cv2.convertScaleAbs(frame, alpha=1.5, beta=0)
+def arrows_detection(frame):
+    '''
+    img = cv2.GaussianBlur(frame, (11, 11), 0)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    corners = cv2.goodFeaturesToTrack(gray, 7, 0.01, 10)
+    corners = np.int0(corners)
+    
+    # Desenhar cantos detectados
+    ct = 'a'
+    for i in corners:
+        x, y = i.ravel()
+        cv2.circle(img, (x, y), 3, (255, 255, 0), -1)
+        cv2.putText(img, ct, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
+        ct = chr(ord(ct) + 1)
+    
+    # Encontrar os valores máximos e mínimos dos cantos
+    xmax, ymax = np.max(corners, axis=0).ravel()
+    xmin, ymin = np.min(corners, axis=0).ravel()
+    
+    # Centro da seta
+    x_center = (xmax + xmin) // 2
+    y_center = (ymax + ymin) // 2
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # Definindo as direções com base na posição dos cantos
+    if abs(xmax - xmin) > abs(ymax - ymin):  # Setas horizontais
+        if np.mean(corners[:, 0, 0]) < x_center:
+            print('RIGHR')
+            direction = 'RIGHT'
+        else:
+            print('LEFT')
+            direction = 'LEFT'
+    else:  # Setas verticais
+        if np.mean(corners[:, 0, 1]) < y_center:
+            print('DOWN')
+            direction = 'DOWN'
+        else:
+            print('UP')
+            direction = 'UP'
+            
+    
+    cv2.imshow("img", img)
 
-    # Define range for the color red
-    lower_red = np.array([160, 60, 60])
-    upper_red = np.array([179, 255, 255])
+    return img, direction
+    '''
+    
+    img = frame
+    img = cv2.GaussianBlur(img, (11,11), 0)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    corners = cv2.goodFeaturesToTrack(gray,7,0.01,10)
+    corners = np.int0(corners)
+    
+    ct='a'
+    for i in corners:
+        x,y = i.ravel()
+        #print(x,y)
+        cv2.circle(img,(x,y),3,(255,255,0),-1)
+        cv2.putText(img, ct, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2, cv2.LINE_AA )
+        ct=ct+'a'
+    
+    cv2.imshow('image',img)
 
-    # Create a filter for the color red
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    xmax, ymax = (np.max(corners, axis = 0)).ravel()
+    xmin, ymin = (np.min(corners, axis = 0)).ravel() 
+    
+       
+    if( abs(xmax-xmin) > abs(ymax-ymin)):
+        if(np.count_nonzero(corners[:,0,0] == xmax) == 2):
+            print('LEFT')
+        else:
+            print('RIGHT')
+    else:
+        if(np.count_nonzero(corners[:,0,1] == ymax) == 2):
+            print('UP')
+        else:
+            print('DOWN')   
+    
+    #cv2.imshow('image',img)
+    return img
+    '''
+    height, width, channels = img.shape 
+    img = cv2.resize(img, (width*8, height*8))                    
+    img = cv2.medianBlur(img,9)
+    imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    # Apply the filter to the image
-    filteredFrame = cv2.bitwise_and(frame, frame, mask=mask)
 
-    # Find contours of red objects
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    ret,th1 = cv2.threshold(imgray,150,255,cv2.THRESH_BINARY)
+    edged=cv2.Canny(th1,127,200)
+    #return edged
 
-    # Filter contours based on area
-    min_contour_area = 1000  # Minimum contour area to consider
-    large_contours = [contour for contour in contours if cv2.contourArea(contour) > min_contour_area]
 
-    for contour in large_contours:
-        # Approximate the contour to a polygon
-        epsilon = 0.022 * cv2.arcLength(contour, True)
-        approx = cv2.approxPolyDP(contour, epsilon, True)
+    (img2,cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        # If the polygon has 3 vertices, it is a triangle
-        if len(approx) == 3:
-            # Draw the contour
-            cv2.drawContours(filteredFrame, [approx], -1, (0, 255, 0), 2)
+    kot=[]
+    up_c=0
+    down_c=0
 
-            # Compute the center of the contour
-            M = cv2.moments(contour)
-            if M["m00"] != 0:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                cv2.circle(filteredFrame, (cX, cY), 3, (255, 255, 255), -1)
 
-    return filteredFrame
 
-tello = Tello()
+    for c in cnts:
+        area = cv2.contourArea(c)
+        #print area
+        cv2.drawContours(img,[c],0,(0,255,0),1)
+        if area > 500 and area < 1650:  
+            #cv2.drawContours(img,[c],0,(0,255,0),1)
+            (x,y),radius = cv2.minEnclosingCircle(c)
+            center = (int(x),int(y))
 
-tello.connect()
-tello.streamon()
+            ellipse = cv2.fitEllipse(c)
+            (x,y),(MA,ma),angle = cv2.fitEllipse(c)
+            #cv2.ellipse(img,ellipse,(0,255,0),1)
 
-while True:
-    frame = tello.get_frame_read().frame
+            rect = cv2.minAreaRect(c)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            #print box
+            cv2.drawContours(img,[box],0,(0,0,255),1)
 
-    # Apply color correction
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # type: ignore
 
-    filteredFrame = detect_triangles(frame)
+            a= math.hypot(box[1][0] - box[0][0], box[1][1] - box[0][1])
+            b= math.hypot(box[3][0] - box[0][0], box[3][1] - box[0][1])
 
-    # Draw grid lines
-    grid_spacing = 50  # Distance between grid lines
-    height, width, _ = frame.shape
+            if a>b: 
+                xos=(box[0][0]+box[1][0])/2
+                yos=(box[0][1]+box[1][1])/2
+                xos2=(box[2][0]+box[3][0])/2
+                yos2=(box[2][1]+box[3][1])/2
 
-    # Draw horizontal lines
-    for y in range(0, height, grid_spacing):
-        cv2.line(filteredFrame, (0, y), (width, y), (255, 0, 0), 1)
+                xosa=(box[1][0]+box[2][0])/2
+                yosa=(box[1][1]+box[2][1])/2
+                xos2a=(box[0][0]+box[3][0])/2
+                yos2a=(box[0][1]+box[3][1])/2
 
-    # Draw vertical lines
-    for x in range(0, width, grid_spacing):
-        cv2.line(filteredFrame, (x, 0), (x, height), (255, 0, 0), 1)
+                bbPath = mplPath.Path(np.array([[box[0][0], box[0][1]],[xos, yos],[xos2, yos2],[box[3][0], box[3][1]]]))
+                bbPath2 = mplPath.Path(np.array([[box[1][0], box[1][1]], [xos, yos], [xos2, yos2],[box[2][0], box[2][1]]]))
 
-    # Show frame with filter and without filter
-    cv2.imshow('frame', frame)
-    cv2.imshow('filteredFrame', filteredFrame)
+                #pol=np.array([[box[1][0], box[1][1]], [xos, yos], [xos2, yos2],[box[2][0], box[2][1]]])
+                #cv2.drawContours(img,[pol],0,(0,0,255),1)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
+            elif b>a:   
+                xos=(box[1][0]+box[2][0])/2
+                yos=(box[1][1]+box[2][1])/2
+                xos2=(box[0][0]+box[3][0])/2
+                yos2=(box[0][1]+box[3][1])/2
+
+                xosa=(box[0][0]+box[1][0])/2
+                yosa=(box[0][1]+box[1][1])/2
+                xos2a=(box[2][0]+box[3][0])/2
+                yos2a=(box[2][1]+box[3][1])/2
+
+                bbPath = mplPath.Path(np.array([[box[0][0], box[0][1]],[box[1][0], box[1][1]], [xos, yos], [xos2, yos2]]))
+                bbPath2 = mplPath.Path(np.array([[box[3][0], box[3][1]],[box[2][0], box[2][1]], [xos, yos], [xos2, yos2]]))
+
+                #pol=np.array([[box[3][0], box[3][1]],[box[2][0], box[2][1]], [xos, yos], [xos2, yos2]])
+                #cv2.drawContours(img,[pol],0,(0,0,255),1)
+
+            r = 5 # accuracy
+            dots_down=[]
+            dots_up=[]
+            for dot in c:
+                result= bbPath.contains_point((dot[0][0], dot[0][1]),radius=r) or bbPath.contains_point((dot[0][0], dot[0][1]),radius=-r)
+                result2= bbPath2.contains_point((dot[0][0], dot[0][1]),radius=r) or bbPath2.contains_point((dot[0][0], dot[0][1]),radius=-r)            
+
+                if result == True :     
+                    nov = [dot[0][0], dot[0][1]]
+                    dots_down.append(nov)
+
+                if result2 == True : 
+                    nov = [dot[0][0], dot[0][1]]
+                    dots_up.append(nov)
+                    cv2.circle(img, (dot[0][0],dot[0][1]), 2, (255, 0, 0), -1)
+                    #print nov
+
+            #if contour is closed
+            if cv2.isContourConvex(c):
+                down=np.array(dots_down)
+                up=np.array(dots_up)
+                area_down = cv2.contourArea(down)
+                area_up = cv2.contourArea(up)
+
+            else:
+                area_down = 20
+                area_up = 1             
+
+
+
+            #print area_up
+            #print area_down
+            #print area_up
+            #print angle
+
+            if area_up > area_down:
+                up_c=up_c+1
+                #kot.append(round(angle,-1))    
+                dx = xosa - xos2a
+                dy = yosa - yos2a
+                rads = atan2(-dy,dx)
+                rads %= 2*pi
+                degs = degrees(rads)
+                #print 'st:'
+                #print round(degs,-1)
+                #print round(angle,-1)
+                kot.append(round(degs,-1))
+
+
+            else:
+                down_c=down_c+1
+                #kot.append(round(angle,-1)) 
+                down_c=down_c+1
+                dx = xos2a - xosa
+                dy = yos2a - yosa
+                rads = atan2(-dy,dx)
+                rads %= 2*pi
+                degs = degrees(rads)
+                #print degs
+                kot.append(round(degs,-1))  
+
+
+            cv2.drawContours(img, [c], -1, (0, 255, 255), 1)
+            #cv2.circle(img, center, 1, (125, 125, 0), -1)
+
+
+
+
+    d = {}
+    if kot:
+        for elm in kot:
+            d[elm] = d.get(elm, 0) + 1
+        counts = [(j,i) for i,j in d.items()]
+        count, max_elm = max(counts)
+
+        #print max_elm
+
+
+        #preracunaj kot
+        if up_c > down_c and max_elm > 180:
+            max_elm=360-max_elm
+
+        elif up_c > down_c:
+            max_elm=max_elm
+
+        elif max_elm < 180:
+            max_elm=max_elm+180
+        else:
+            max_elm=max_elm
+
+
+        #print max_elm
+
+
+        if max_elm >= 337.5 and max_elm < 360:
+            smer = 'W'
+        elif max_elm >=0 and max_elm <22.5:
+            smer = 'W'
+        elif max_elm >=22.5 and max_elm <67.5:
+            smer = 'SE'
+        elif max_elm >=67.5 and max_elm <112.5:
+            smer = 'S'
+        elif max_elm >=112.5 and max_elm <157.5:
+            smer = 'SW'
+        elif max_elm >=157.5 and max_elm <202.5:
+            smer = 'E'
+        elif max_elm >=202.5 and max_elm <247.5:
+            smer = 'NE'
+        elif max_elm >=247.5 and max_elm <292.5:
+            smer = 'N'
+        elif max_elm >=292.5 and max_elm <337.5:
+            smer = 'NW'
+        else:
+            smer=0
+
+        print(smer)
+        return smer
+    '''
+
+if __name__ == "__main__":
+    # open an image file
+    img = cv2.imread('arrow.jpg')
+    arrows_detection(img)
+    cv2.waitKey(0)
