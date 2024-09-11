3b59e8ef58cc3e7606aafb74907da5f9c0c0d742
Marcos Seiji Shiba <marcos.shiba@unifesp.br>
2024-09-11 14:56:40 -0300

    Fixed bug in danger

--- Principal/danger.py
+++ Principal/danger.py
@@ -15,23 +15,26 @@ def danger(end_flag):
     dir = os.path.join(dir, "images")
     while not end_flag:
         frames = [f for f in os.listdir(dir) if f.endswith('.png')]
-        
-        with open("read_frames.txt", "a") as f:
-            for frame in frames:
-                if frame in f.read():
-                    pass
-                else:
-                    detect_flag = detect_danger(frame)
-                    f.write(frame + "\n")
-                    if detect_flag:
-                        frame_name = frame.split(".")[0]
-                        send_alert(frame_name)
-                        with open("alert_log.txt", "a") as log:
-                            log.write(frame_name + "\n")
-                    # Remove frames if more than 10 frames have been read
-                    if len(f.readlines()) > 10:
-                        files = f.readlines()
-                    for file in files:
-                        os.remove(os.path.join(dir, file))
-                    f.seek(0)
-                    f.truncate()
+        try:
+            with open("read_frames.txt", "r") as f:
+                for frame in frames:
+                    if frame in f.read():
+                        pass
+                    else:
+                        detect_flag = detect_danger(frame)
+                        with open("read_frames.txt", "a") as f:
+                            f.write(frame + "\n")
+                        if detect_flag:
+                            frame_name = frame.split(".")[0]
+                            send_alert(frame_name)
+                            with open("alert_log.txt", "a") as log:
+                                log.write(frame_name + "\n")
+                        # Remove frames if more than 10 frames have been read
+                        if len(f.readlines()) > 10:
+                            files = f.readlines()
+                        for file in files:
+                            os.remove(os.path.join(dir, file))
+                        f.seek(0)
+                        f.truncate()
+        except:
+            open("read_frames.txt", "w").close()
