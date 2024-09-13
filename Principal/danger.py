a863ce42ef43359865c28117a0838b22ed828e6f
Marcos Seiji Shiba <marcos.shiba@unifesp.br>
2024-09-13 01:15:05 -0300

    Danger finalized

--- Principal/danger.py
+++ Principal/danger.py
@@ -1,6 +1,9 @@
+from calendar import c
 import os
+import re
 
 import cv2
+from numpy import append
 
 def detect_danger(file):
     return True
@@ -14,32 +17,29 @@ def danger(end_flag):
     if not "images" in os.listdir(dir):
         os.mkdir("images")
     dir = os.path.join(dir, "images\\")
+    read_frames = []
+    corrected_lines = []
     while not end_flag:
         frames = [f for f in os.listdir(dir) if f.endswith('.png')]
         try:
-            with open(dir + "read_frames.txt", "r") as f:
+            with open(dir + "alert_log.txt", "r") as f:
+                lines = f.readlines()
+                for line in lines:
+                    line = line.split("\n")[0]
+                    corrected_lines.append(line)
+                    
                 for frame in frames:
-                    if frame in f.read():
-                        print(f.read())
-                        pass
-                    else:
-                        detect_flag = detect_danger(frame)
-                        with open(dir + "read_frames.txt", "a") as f:
-                            f.write(frame + "\n")
-                        if detect_flag:
-                            frame_name = frame.split(".")[0]
-                            send_alert(frame_name)
-                            with open(dir + "alert_log.txt", "a") as log:
-                                log.write(frame_name + "\n")
-                        # Remove frames if more than 10 frames have been read
-                        if len(f.readlines()) > 10:
-                            files = f.readlines()
-                        for file in files:
-                            os.remove(os.path.join(dir, file))
-                        f.seek(0)
-                        f.truncate()
+                    if frame not in corrected_lines:
+                        read_frames.append(frame)
+                        if detect_danger(frame):
+                            send_alert(frame)
+                
+            with open(dir + "alert_log.txt", "a") as f:
+                for frame in read_frames:
+                    f.write(frame + "\n")
+                read_frames = []
+                    
         except:
             open(dir + "read_frames.txt", "w").close()
-            
             
 danger(False)
